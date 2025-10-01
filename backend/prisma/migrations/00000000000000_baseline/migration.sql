5c096e1ef99b4a2f46bb196b2a48655bb43640a8
chujiann.mok <chujiann.mok@atoz-software.tech>
2025-10-01 17:08:58 +0800

    point after purge everything

--- backend/prisma/migrations/00000000000000_baseline/migration.sql
+++ backend/prisma/migrations/00000000000000_baseline/migration.sql
@@ -5,6 +5,28 @@ CREATE TYPE "Status" AS ENUM ('ENABLED', 'DISABLED', 'BANNED');
 CREATE TYPE "MenuType" AS ENUM ('directory', 'menu');
 
 -- CreateTable
+CREATE TABLE "sys_tokens" (
+    "id" TEXT NOT NULL,
+    "access_token" TEXT NOT NULL,
+    "refresh_token" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "domain" TEXT NOT NULL,
+    "login_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ip" TEXT NOT NULL,
+    "port" INTEGER,
+    "address" TEXT NOT NULL,
+    "user_agent" TEXT NOT NULL,
+    "request_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" TEXT NOT NULL,
+
+    CONSTRAINT "sys_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "sys_user" (
     "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
@@ -189,6 +211,26 @@ CREATE TABLE "sys_role_menu" (
     CONSTRAINT "sys_role_menu_pkey" PRIMARY KEY ("role_id","menu_id","domain")
 );
 
+-- CreateTable
+CREATE TABLE "sys_access_key" (
+    "id" TEXT NOT NULL,
+    "domain" TEXT NOT NULL,
+    "access_key_id" TEXT NOT NULL,
+    "access_key_secret" TEXT NOT NULL,
+    "status" "Status" NOT NULL,
+    "description" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" TEXT NOT NULL,
+
+    CONSTRAINT "sys_access_key_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sys_tokens_access_token_key" ON "sys_tokens"("access_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sys_tokens_refresh_token_key" ON "sys_tokens"("refresh_token");
+
 -- CreateIndex
 CREATE UNIQUE INDEX "sys_user_username_key" ON "sys_user"("username");
 
@@ -209,3 +251,10 @@ CREATE UNIQUE INDEX "sys_organization_code_key" ON "sys_organization"("code");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "sys_menu_route_name_key" ON "sys_menu"("route_name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sys_access_key_access_key_id_key" ON "sys_access_key"("access_key_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sys_access_key_access_key_secret_key" ON "sys_access_key"("access_key_secret");
+
